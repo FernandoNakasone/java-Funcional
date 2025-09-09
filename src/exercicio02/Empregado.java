e80a7e54b70bd75441f35f0ce176cc55c9b4ab54
FernandoNakasone <fernandonakasone040@gmail.com>
2025-09-09 11:39:18 -0300

    começo do ex 2

---
+++ src/exercicio02/Empregado.java
@@ -0,0 +1,7 @@
+package exercicio02;
+
+public class Empregado {
+    private String nome;
+    private double salarioAtual;
+    private int anosExperiencia;
+}
---
+++ src/exercicio02/mainEx02.java
@@ -0,0 +1,7 @@
+package exercicio02;
+
+public class mainEx02 {
+    public static void main(String[] args) {
+
+    }
+}
